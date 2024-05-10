444b04af132cc841e517bf306330ce304062b379
Karen <158315598+nogueiiiiiiiiira@users.noreply.github.com>
2024-05-10 15:42:24 -0300

    Update routes.js

--- src/controller/routes.js
+++ src/controller/routes.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const {
+    getBooks, 
+    getBook, 
+    postBook
+} = require('./bookController.js');
 
+const router = express.Router();
+
+router.get('/books', getBooks);
+router.get('/book/:bookId', getBook);
+router.post('/book', postBook);
+
+module.exports = router;
