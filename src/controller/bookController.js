1821799eaf0f57ac55e42712d196d63be3cc0a7b
nogueiiiiiiiiira <karencristininogueira@gmail.com>
2024-05-13 16:51:17 -0300

    Começando librarian

--- src/controller/bookController.js
+++ src/controller/bookController.js
@@ -6,9 +6,19 @@ const {
     listBookById,
     listBookByName,
     updateBookService,
-    deleteBookService
+    deleteBookService,
+
+    addLibrarian,
+    listLibrarians,
+    listLibrarianById,
+    listLibrarianByName,
+    updateLibrarianService,
+    deleteLibrarianService,
+
+
 } = require("../service/bookService");
 
+//Books
 async function getBooks(req, res) {
     const books = await listBooks();
     if (books.length > 0) {
@@ -70,6 +80,91 @@ async function updateBook(req, res) {
         
         const updatedBook = await updateBookService(bookId, nome, descricao, autor, valor, categoria); // Passando o bookId como o primeiro parâmetro
         return res.status(200).json(updatedBook);
+    } catch (error) {
+        console.error('Erro ao atualizar o livro:', error);
+        return res.status(500).json({ message: 'Erro ao atualizar o livro.' });
+    }
+}
+
+async function deleteBook(req, res) {
+    const { bookId } = req.params;
+    try {
+        // Verifique se o livro existe
+        const existingBook = await listBookById(bookId); // Corrigido para listBookById
+        if (!existingBook) {
+            return res.status(404).json({ message: 'Livro não encontrado.' });
+        }
+        // Exclua o livro
+        await deleteBookService(bookId);
+        return res.status(200).json({ message: 'Livro excluído com sucesso.' });
+    } catch (error) {
+        console.error('Erro ao excluir o livro:', error);
+        return res.status(500).json({ message: 'Erro interno do servidor.' });
+    }
+}
+
+//Librarians
+async function getLibrarians(req, res) {
+    const librarians = await listLibrarians();
+    if (librarians.length > 0) {
+        return res.status(200).json(librarians);
+    }
+    return res.status(204).send();
+}
+
+async function getLibrarianById(req, res) {
+    const { librarianId } = req.params;
+    if (!librarianId) {
+        return res.status(400).json({ message: 'ID é obrigatório' });
+    }
+    const result = await listLibrarianById(librarianId);
+    if (result) {
+        return res.status(200).json(result);
+    }
+    return res.status(204).send();
+}
+
+async function getLibrarianByName(req, res) {
+    const { nome } = req.params;
+    if (!nome) {
+        return res.status(400).json({ message: 'Nome do bibliotecário é obrigatório.' });
+    }
+    const result = await listLibrarianByName(nome);
+    if (result && result.length > 0) {
+        return res.status(200).json(result);
+    }
+    return res.status(404).json({ message: 'Nenhum bibliotecário encontrado com este nome.' });
+}
+
+async function postLibrarian(req, res) {
+    const { nome, cpf, email, telefone, dataNasc } = req.body;
+    if (!nome || !cpf || !email || !telefone || !dataNasc) {
+        return res.status(400).json({ message: 'Todos os campos são obrigatórios.' });
+    }
+
+    const result = await addLibrarian(nome, cpf, email, telefone, dataNasc);
+    if (result) {
+        return res.status(201).json({ message: 'Bibliotecário adicionado com sucesso.' });
+    }
+    return res.status(500).json({ message: 'Erro ao adicionar o Bibliotecário.' });
+}
+
+async function updateLibrarian(req, res) {
+    const { nome, cpf, email, telefone, dataNasc } = req.body;
+    const { librarianId } = req.params;
+    
+    if (!nome || !cpf || !email || !telefone || !dataNasc) {
+        return res.status(400).json({ message: 'Todos os campos são obrigatórios.' });
+    }
+    
+    try {
+        const existingLibrarian = await listLibrarianById(librarianId);
+        if (!existingLibrarian) {
+            return res.status(404).json({ message: 'Bibliotecário não encontrado.' });
+        }
+        
+        const updatedLibrarian = await updateBookService(bookId, nome, descricao, autor, valor, categoria); // Passando o bookId como o primeiro parâmetro
+        return res.status(200).json(updatedLibrarian);
     } catch (error) {
         console.error('Erro ao atualizar o livro:', error);
         return res.status(500).json({ message: 'Erro ao atualizar o livro.' });
--- src/controller/routes.js
+++ src/controller/routes.js
@@ -7,16 +7,30 @@ const {
     getBookByName,
     postBook,
     updateBook,
-    deleteBook
+    deleteBook,
+
+    getLibrarians,
+    getLibrarianById,
+    getLibrarianByName,
+    postLibrarian,
+    updateLibrarian,
+    deleteLibrarian
 } = require('./bookController.js');
 
 const router = express.Router();
 
 router.get('/books', getBooks);
 router.get('/book/:bookId', getBookById);
-router.get('/bookByName/:nome', getBookByName); // Alterado de bookName para nome
+router.get('/bookByName/:nome', getBookByName); 
 router.post('/book', postBook);
 router.put('/book/:bookId', updateBook);
 router.delete('/book/:bookId', deleteBook);
+
+router.get('/librarians', getLibrarians);
+router.get('/librarian/:librarianId', getLibrarianById);
+router.get('/librarianByName/:nome', getLibrarianByName);
+router.post('/librarian', postLibrarian);
+router.put('/librarian/:librarianId', updateLibrarian);
+router.delete('/librarian/:librarianId', deleteLibrarian);
 
 module.exports = router;
