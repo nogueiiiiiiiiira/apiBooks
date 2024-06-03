5e836e485d5d46f4ad19bb1c04d347ab7633a304
nogueiiiiiiiiira <karencristininogueira@gmail.com>
2024-06-03 15:33:55 -0300

    CORS

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ const routes = require('./controller/routes');
 const app = express();
 const PORT = 3001;
 
+const cors = require('cors');
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(routes);
 
